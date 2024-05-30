8eb009c3c946ad56ac10f0d267f8b5a071b39144
Cesar1xd1 <cesar1xd1@gmail.com>
2024-05-29 20:02:19 -0600

    Se probo solucionar clonando manualmente el proyecto y haciendo build

---
+++ src/db.js
@@ -0,0 +1,13 @@
+import {connect} from 'mongoose';
+
+
+
+(async ()=>{
+    try {
+        const db = await connect("mongodb+srv://cesar2xd2:cesar2xd2@clusterxd.a5efmkr.mongodb.net/?retryWrites=true&w=majority&appName=ClusterXD");
+        console.log("BD conexion to ", db.connection.name);
+    } catch (error) {
+        console.error(error);
+    }
+    
+})();
