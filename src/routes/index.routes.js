767910b48635b4e6b37bfdfb0f9750846a6b2117
Cesar1xd1 <cesar1xd1@gmail.com>
2024-05-30 19:46:29 -0600

    se regreso las vistas y las rutas

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,46 +1,32 @@
 import {Router} from 'express';
-import { deleteAuto, getEditA, insertarAuto, postEditA,renderIndexA,renderTablaA } from '../controllers/auto.controlador';
-import { deleteCliente, getEditC, insertarCliente, postEditC,renderTablaC } from '../controllers/cliente.controlador';
+import { deleteAuto, getEdit, insertarAuto, postEdit, renderIndex,renderTabla} from '../controllers/auto.controlador';
 
 const router = Router();
 //Index
-router.get('/',renderIndexA );
+router.get('/',renderIndex );
 
-router.get('/tabla', renderTablaA);
-router.get('/clientes', renderTablaC);
-
-
+router.get('/tabla', renderTabla);
 
 //ALTAS
 router.get('/altas',(req,res)=>{
     res.render('altas');
 });
-router.get('/altasC',(req,res)=>{
-    res.render('altasC');
-});
 
-//render
 router.get('/tabla',(req,res)=>{
     res.render('tabla');
-});
-router.get('/clientes',(req,res)=>{
-    res.render('clientes');
 });
 
 
 router.post('/autos/add',insertarAuto);
-router.post('/clientes/add',insertarCliente);
 
 //BAJAS
 router.get('/autos/:id/delete', deleteAuto);
-router.get('/clientes/:id/deleteC', deleteCliente);
+
+
 
 //CAMBIOS
-router.get('/autos/:id/edit', getEditA);
-router.post('/autos/:id/edit' ,postEditA);
-
-router.get('/clientes/:id/editC', getEditC);
-router.post('/clientes/:id/editC' ,postEditC);
+router.get('/autos/:id/edit', getEdit);
+router.post('/autos/:id/edit' ,postEdit);
 
 //Export
 export default router;
